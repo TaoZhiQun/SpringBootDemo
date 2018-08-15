ae80176ea47c231480474a931ad622bc40e9a84d
TaoZhiQun <1662302600@qq.com>
2018-08-15 22:50:53 +0800

    添加单机redis

---
+++ src/main/java/com/example/impl/RedisSingleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.impl;
+
+import com.example.service.RedisService;
+import org.springframework.stereotype.Service;
+
+@Service(value = "redisService")
+public class RedisSingleServiceImpl implements RedisService {
+
+}
