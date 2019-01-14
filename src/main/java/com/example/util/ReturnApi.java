df10b7518b322e434b7c49b7cc7359455917206a
taozq <taozq@quantdo.com.cn>
2019-01-14 16:02:19 +0800

    修改返回消息类

--- src/main/java/com/example/util/ReturnApi.java
+++ src/main/java/com/example/util/ReturnApi.java
@@ -5,15 +5,30 @@ import java.util.List;
 import java.util.Map;
 
 public class ReturnApi {
+
+    public Map<String,Object> returnBooleanContent(boolean code, String msg){
+        Map map = new HashMap<>(2);
+        map.put("code",code);
+        map.put("msg",msg);
+        return map;
+    }
+
+    public Map<String,String> returnStringContent(String code, String msg){
+        Map map = new HashMap<>(2);
+        map.put("code",code);
+        map.put("msg",msg);
+        return map;
+    }
+
     public Map<String,Object> returnJson(int code, String msg){
-        Map map = new HashMap();
+        Map map = new HashMap<>(2);
         map.put("code",code);
         map.put("msg",msg);
         return map;
    }
 
    public Map<String,Object> returnJson(int code,String msg,List<Map> data){
-       Map map=new HashMap();
+       Map map = new HashMap<>(2);
        map.put("code",code);
        map.put("msg",msg);
        map.put("data",data);
